4adeccb6a6205a24b1e7d4b742fed6e027325c54
Iulian Dogariu <code@iuliandogariu.com>
2010-10-03 03:33:15 +0300

    cleanup test

--- spec/deak/book_spec.rb
+++ spec/deak/book_spec.rb
@@ -19,38 +19,41 @@ module Deak
       act_groceries.total_increase.should == 120
     end
   
-    it "should reflect the difference between balance and total increase" do
-      act_owner_equity = @book.add_account!( :name => "Owner Equity",
-                                             :debit_is_decrease => true ) 
-      act_cash         = @book.add_account!( :name => "Cash" )
-      @book.add_transaction!( :amount => "1000",
-                              :decrease_account => act_owner_equity,
-                              :increase_account => act_cash )
-      act_owner_equity.balance.       should == 1000
-      act_owner_equity.total_increase.should == -1000
-      act_cash.        balance.       should == 1000
-      act_cash.        total_increase.should == 1000
-    end
+    describe "debits, credits, and balance" do
 
-    it "should add a debit/credit between two asset accounts" do
-      act_cash      = @book.add_account!( :name => "Cash" ) 
-      act_groceries = @book.add_account!( :name => "Groceries" )
-      @book.add_transaction!( :amount => "120",
-                              :debit_account  => act_groceries,
-                              :credit_account => act_cash )
-      act_groceries.balance.should == 120
-      act_cash.balance.should      == -120
-    end
+      before(:each) do
+        @act_cash         = @book.add_account!( :name => "Cash" )
+        @act_groceries    = @book.add_account!( :name => "Groceries" )
+        @act_owner_equity = @book.add_account!( :name => "Owner Equity",
+                                                :debit_is_decrease => true ) 
+      end
 
-    it "should add a debit/credit between asset and equity account" do
-      act_owner_equity = @book.add_account!( :name => "Owner Equity",
-                                             :debit_is_decrease => true ) 
-      act_cash         = @book.add_account!( :name => "Cash" )
-      @book.add_transaction!( :amount => "1000",
-                              :debit_account  => act_cash,
-                              :credit_account => act_owner_equity )
-      act_owner_equity.balance.should == 1000
-      act_cash.        balance.should == 1000
+      it "should reflect the difference between balance and total increase" do
+        @book.add_transaction!( :amount => "1000",
+                                :decrease_account => @act_owner_equity,
+                                :increase_account => @act_cash )
+        @act_owner_equity.balance.       should == 1000
+        @act_owner_equity.total_increase.should == -1000
+        @act_cash.        balance.       should == 1000
+        @act_cash.        total_increase.should == 1000
+      end
+
+      it "should add a debit/credit between two asset accounts" do
+        @book.add_transaction!( :amount => "120",
+                                :debit_account  => @act_groceries,
+                                :credit_account => @act_cash )
+        @act_groceries.balance.should == 120
+        @act_cash.balance.should      == -120
+      end
+
+      it "should add a debit/credit between asset and equity account" do
+        @book.add_transaction!( :amount => "1000",
+                                :debit_account  => @act_cash,
+                                :credit_account => @act_owner_equity )
+        @act_owner_equity.balance.should == 1000
+        @act_cash.        balance.should == 1000
+      end
+
     end
 
   end
